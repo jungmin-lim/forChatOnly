d18def773724fdf5524e0a6792f28ac5439cea3f
eungu <kiwi9084@gmail.com>
2020-12-04 20:03:52 +0900

    add choose_item

--- screen.h
+++ screen.h
@@ -1,17 +1,15 @@
 #ifndef _SCREEN_H_
 #define _SCREEN_H_
 
+#define NAMESZ 64
+
 int exit_handler();
 void init_screen();
-void init_colorset();
 void add_bubble(char* name, char* msg, int color);
 int getstring(char* buf, int isChat);
-void clear_inputspace();
-void init_inputspace();
-void reset_inputfield(char*, int, int);
 void init_remote();
 void end_remote();
 void print_remote(char* msg);
-void reset_remotefield(char* msg, int *starty, int startx, int y, int x);
+int choose_from_array(int count, int* id, char name[][NAMESZ]);
 
 #endif
